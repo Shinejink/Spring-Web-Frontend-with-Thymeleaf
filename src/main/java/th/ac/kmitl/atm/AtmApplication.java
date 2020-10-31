de11da831e4a343defd83b6fba486990551d8909
ปรเมศวร์ โค้นสากรม 61050854 <72653768+Shinejink@users.noreply.github.com>
2020-10-31 19:00:16 +0700

    Add files via upload
    ปรเมศวร์ โค้นสากรม 61050854

---
+++ src/main/java/th/ac/kmitl/atm/AtmApplication.java
@@ -0,0 +1,13 @@
+package th.ac.kmitl.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//ปรเมศวร์ โค้นสากรม 61050854//
+@SpringBootApplication
+public class AtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmApplication.class, args);
+	}
+
+}
